277ac8a85daa2882042380f255bc45a940b0a99c
DarkseidAyansh <ayanshsaroj94@gmail.com>
2024-11-03 09:57:07 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/nativeNomads/workhive/UserDashboard/Adapter/JobInfo.kt
+++ app/src/main/java/com/nativeNomads/workhive/UserDashboard/Adapter/JobInfo.kt
@@ -1,7 +1,6 @@
 package com.nativeNomads.workhive.UserDashboard.Adapter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,12 +9,13 @@ import com.nativeNomads.workhive.R
 class JobInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_job_info)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
+        // Apply window insets to adjust padding for system bars
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
--- app/src/main/java/com/nativeNomads/workhive/UserDashboard/Adapter/JobListingAdapter.kt
+++ app/src/main/java/com/nativeNomads/workhive/UserDashboard/Adapter/JobListingAdapter.kt
@@ -1,53 +1,46 @@
 package com.nativeNomads.workhive.UserDashboard.Adapter
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.cardview.widget.CardView
-
 import androidx.recyclerview.widget.RecyclerView
 import com.nativeNomads.workhive.R
-import android.app.job.JobInfo
 
 class JobListingAdapter(
     var companyName: ArrayList<String>,
     var jobTitle: ArrayList<String>,
-    var context: Context):RecyclerView.Adapter<JobListingAdapter.ViewHolder>()
+    var context: Context
+) : RecyclerView.Adapter<JobListingAdapter.ViewHolder>() {
 
-{
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        var textViewCompanyName:TextView=itemView.findViewById(R.id.companyName)
-        var textViewJobTitle:TextView=itemView.findViewById(R.id.jobDescribe)
-        var cardView:CardView=itemView.findViewById(R.id.cardView)
-        var moreInfo:Button=itemView.findViewById(R.id.more)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textViewCompanyName: TextView = itemView.findViewById(R.id.companyName)
+        var textViewJobTitle: TextView = itemView.findViewById(R.id.jobDescribe)
+        var cardView: CardView = itemView.findViewById(R.id.cardView)
+        var moreInfo: Button = itemView.findViewById(R.id.more)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view:View=LayoutInflater.from(parent.context).inflate(R.layout.job_listing,parent,false)
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.job_listing, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textViewCompanyName.text= companyName[position]
-        holder.textViewJobTitle.text= jobTitle[position]
+        holder.textViewCompanyName.text = companyName[position]
+        holder.textViewJobTitle.text = jobTitle[position]
 
-//
-        holder.moreInfo.setOnClickListener{
-            val intent= Intent(context as Activity,JobInfo::class.java)
-            (context as Activity).startActivity(intent)
+        // Set up the click listener for the "More Info" button
+        holder.moreInfo.setOnClickListener {
+            val intent = Intent(context, JobInfo::class.java)
+            context.startActivity(intent)
         }
-
     }
 
     override fun getItemCount(): Int {
         return companyName.size
     }
-
-
 }
